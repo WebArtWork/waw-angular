736e3399e0ba518e657a41ce8dfdb5888cf53edf
dmaksiuta <shutle369@gmail.com>
2021-03-15 20:25:54 +0200

    fixed core module in pages

--- page/table/module.ts
+++ page/table/module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@common';
+import { CoreModule } from '@core';
 import { CNAMEComponent } from './NAME.component';
 import { Routes, RouterModule } from '@angular/router';
 
@@ -11,7 +11,7 @@ const routes: Routes = [{
 @NgModule({
 	imports: [
 		RouterModule.forChild(routes),
-		CommonModule
+		CoreModule
 	],
 	declarations: [
 		CNAMEComponent
