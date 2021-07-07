46a2241e3b48954b609615f008a54a83e26e678a
Honchar Denys <crackeraki@gmail.com>
2021-07-07 22:35:43 +0300

    fixed service generation

--- service/service.ts
+++ service/service.ts
@@ -6,41 +6,42 @@ import { MongoService, AlertService } from 'wacom';
 })
 export class CNAMEService {
 	public NAMEs: any = [];
+	public _NAMEs: any = {};
 	constructor(private mongo: MongoService, private alert: AlertService) { 
-		this.NAMEs = mongo.get('NAME');
+		this.NAMEs = mongo.get('NAME', {}, (arr, obj)=>{
+			this._NAMEs = obj;
+		});
 	}
-	create(NAME:any={}) {
+	create(NAME:any={}, text = 'NAME has been created.') {
 		if(NAME._id) return this.save(NAME);
 		this.mongo.create('NAME', NAME, created=>{
-			this.alert.show({
-				text: 'NAME has been created.'
-			});
+			this.alert.show({ text });
 		}); 
 	}
 	doc(NAMEId){
-		return this.mongo.fetch('NAME', {
-			query: {
-				_id: NAMEId
-			}
-		});
+		if(!this._NAMEs[NAMEId]){
+			this._NAMEs[NAMEId] = this.mongo.fetch('NAME', {
+				query: {
+					_id: NAMEId
+				}
+			});
+		}
+		return this._NAMEs[NAMEId];
+		
 	}
-	update(NAME) {
+	update(NAME, text = 'NAME has been updated.') {
 		this.mongo.afterWhile(NAME, ()=> {
 			this.save(NAME);
 		});
 	}
-	save(NAME){
+	save(NAME, text = 'NAME has been updated.'){
 		this.mongo.update('NAME', NAME, ()=>{
-			this.alert.show({
-				text: 'NAME has been updated.'
-			});
+			this.alert.show({ text });
 		});
 	}
-	delete(NAME) {
+	delete(NAME, text = 'NAME has been deleted.') {
 		this.mongo.delete('NAME', NAME, ()=>{
-			this.alert.show({
-				text: 'NAME has been deleted.'
-			});
+			if(text) this.alert.show({ text });
 		});
 	}
 }
