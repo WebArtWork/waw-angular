d5b472c347b2f73c9b62cab8d7ddcd624d0913c2
Nika <nikarogovska@gmail.com>
2023-09-19 11:50:48 +0300

    fixed import core module

--- page/crud/module.ts
+++ page/crud/module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { CoreModule } from 'src/app/core';
+import { CoreModule } from 'src/app/core/core.module';
 import { CNAMEComponent } from './NAME.component';
 import { Routes, RouterModule } from '@angular/router';
 
