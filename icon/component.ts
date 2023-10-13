e51645612afb76f86d08656daebb5087ab13f3a8
Nika <129855721+Nika-cyber1@users.noreply.github.com>
2023-10-13 14:18:11 +0300

    Update component.ts

--- icon/component.ts
+++ icon/component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-@Input() color = 'black';
 @Component({
 	selector: 'NAME-icon',
 	templateUrl: './FILENAME.component.html',
 	styleUrls: ['./FILENAME.component.scss']
 })
-export class CNAMEComponent {}
+export class CNAMEComponent {
+	@Input() color = 'black';
+}
