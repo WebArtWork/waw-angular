abfd0332a55bbee4c9c6273865e77c9949db70d9
Nika <129855721+Nika-cyber1@users.noreply.github.com>
2023-10-13 12:27:43 +0300

    Create component.ts

---
+++ icon/component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+@Component({
+	selector: 'core-NAME',
+	templateUrl: './FILENAME.component.html',
+	styleUrls: ['./FILENAME.component.scss']
+})
+export class CNAMEComponent {}
