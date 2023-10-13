1f747523810185617005fe4be4f0738fc6c33872
Nika <129855721+Nika-cyber1@users.noreply.github.com>
2023-10-13 15:24:19 +0300

    Update component.ts

--- formcomponents/default/component.ts
+++ formcomponents/default/component.ts
@@ -1,1 +1,8 @@
-
+import { Component } from '@angular/core';
+@Component({
+	selector: 'NAME-formcomponents',
+	templateUrl: './FILENAME.component.html',
+	styleUrls: ['./FILENAME.component.scss']
+})
+export class CNAMEComponent {
+}
