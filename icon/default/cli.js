faf31e7d90b50e6f702ed45d2c5af461c1859a14
Nika <129855721+Nika-cyber1@users.noreply.github.com>
2023-10-30 16:36:32 +0200

    Update cli.js

--- icon/default/cli.js
+++ icon/default/cli.js
@@ -9,8 +9,15 @@ module.exports = async (waw) => {
 		"https://webart.work/api/registry/ngx/icon/" + waw.name
 	);
 
+		let resp;
 	if (response.ok) {
-		const resp = await response.json();
+		resp = await response.json();
+	}
+	
+	if (response.ok && resp) {
+		fs.mkdirSync(waw.base, {
+			recursive: true
+		});
 
 		if (resp.repo) {
 			waw.fetch(waw.base, resp.repo, (err) => {}, resp.branch || 'master');
